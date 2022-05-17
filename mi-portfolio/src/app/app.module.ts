d9dfdf7acdece46a65f54d558b6789b9e81df128
Rodriguez Plana <jprplana@gmail.com>
2022-05-17 20:44:06 -0300

    Se integra a Angular

--- mi-portfolio/src/app/app.module.ts
+++ mi-portfolio/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { PrimaryCardComponent } from './componentes/primary-card/primary-card.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { RedesComponent } from './componentes/header/redes/redes.component';
+import { LogosAPComponent } from './componentes/header/logos-ap/logos-ap.component';
+import { LoginComponent } from './componentes/header/login/login.component';
+import { NavRightComponent } from './componentes/header/nav-right/nav-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrimaryCardComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+    RedesComponent,
+    LogosAPComponent,
+    LoginComponent,
+    NavRightComponent
   ],
   imports: [
     BrowserModule,
