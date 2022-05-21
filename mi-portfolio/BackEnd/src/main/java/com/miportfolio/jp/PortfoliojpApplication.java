9baee24dc3be768d1cda07a61f2a58f5b356562a
Rodriguez Plana <jprplana@gmail.com>
2022-05-21 15:51:14 -0300

    Agrega carpeta BackEnd al repo

---
+++ mi-portfolio/BackEnd/src/main/java/com/miportfolio/jp/PortfoliojpApplication.java
@@ -0,0 +1,13 @@
+package com.miportfolio.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliojpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliojpApplication.class, args);
+	}
+
+}
