15674223380f591692fe46652ed7503b7b83c893
Rodriguez Plana <jprplana@gmail.com>
2022-07-29 20:27:07 -0300

    Agrega funcionalidad a Estudios

--- mi-portfolio/FrontEnd/src/app/app.module.ts
+++ mi-portfolio/FrontEnd/src/app/app.module.ts
@@ -29,6 +29,9 @@ import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './componentes/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './componentes/experiencia/edit-experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NeweducacionComponent } from './componentes/educacion/neweducacion.component';
+import { EditEducacionComponent } from './componentes/educacion/edit-educacion.component';
 
 
 
@@ -55,7 +58,10 @@ import { EditExperienciaComponent } from './componentes/experiencia/edit-experie
     AddSectionsComponent,
     HomeComponent,
     NewExperienciaComponent,
-    EditExperienciaComponent
+    EditExperienciaComponent,
+    EducacionComponent,
+    NeweducacionComponent,
+    EditEducacionComponent
   ],
   imports: [
     BrowserModule,
